ca79d867ca2571a7ddef4b914b0d97f9e67424f9
Matheus Grossi <matheusgrossi2000@gmail.com>
2026-03-30 09:17:06 -0300

    Adição da primeira versão funcional, na sequência irei implementar melhorias e adição de um novo led para emergência

--- dev_wip/PIC32MK_on_off_button.X/src/main.c
+++ dev_wip/PIC32MK_on_off_button.X/src/main.c
@@ -14,26 +14,41 @@
 
 int state = 0;
 
-void main()
-{
-    //CMCON = 0x07;
+int init(void){
+
     CFGCONbits.JTAGEN = 0;
 
     TRISBbits.TRISB1 = 1;
     TRISBbits.TRISB2 = 1;
     TRISBbits.TRISB10 = 0;
+    TRISBbits.TRISB13 = 0;
     
-    PORTBbits.RB10 = 0;
+    ANSELBbits.ANSB1 = 0;
+    ANSELBbits.ANSB2 = 0;
 
+    //RB10 e RB13 nao tem modo analogico
+    //ANSELBbits.ANSB10 = 0;
+    //ANSELBbits.ANSB13 = 0;
+    
+    LATBbits.LATB10 = 0;
+    LATBbits.LATB13 = 1;
+}
+
+int main(void)
+{
+    init();
+    
     while(1){
-        if ( (PORTBbits.RB1) && (!PORTBbits.RB2) ) { // Botao pressionado
+        if ( PORTBbits.RB1 ) { // Opera��o de liga
             delay_ms(500);
-            PORTBbits.RB10 = 1;
+            LATBbits.LATB10 = 0;
+            LATBbits.LATB13 = 1;
         }
         else
-        if ( (!PORTBbits.RB1) && (PORTBbits.RB2) ) { // Botao solto
+        if ( PORTBbits.RB2 ) { // Opera��o de desliga
             delay_ms(500);
-            PORTBbits.RB10 = 0;
+            LATBbits.LATB10 = 1;
+            LATBbits.LATB13 = 0;
         }
     }
 }
